4a120bd8d88f48e7b5928deaab7e8894f170e514
kaju111 <saikiakaju275@gmail.com>
2024-03-30 20:51:35 +0530

    Done

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,24 @@
+import { User } from "../models/user.js";
+
+// Create a new user and save it to the database and return a response and save in cookie
+const newUser = async (req, res) => {
+  const avatar = {
+    public_id: "ajnlmas",
+    url: "skablks",
+  };
+
+  await User.create({
+    name: "Chaman",
+    username: "chaman",
+    password: "chaman",
+    avatar,
+  });
+
+  res.status(201).json({ message: "User created successfully" });
+};
+
 const login = (req, res) => {
   res.send("Hello World");
 };
-export { login };
+
+export { login, newUser };
