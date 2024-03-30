abe096b57a88c03302541b53a8f42daccc682229
kaju111 <saikiakaju275@gmail.com>
2024-03-30 20:15:22 +0530

    Done

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 import express from "express";
-import { connectDB } from "./utils/features.js";
-import dotenv from "dotenv";
-import { errorMiddlewares } from "./middlewares/error.js";
-import cookieParser from "cookie-parser";
+// import { connectDB } from "./utils/features.js";
+// import dotenv from "dotenv";
+// import { errorMiddlewares } from "./middlewares/error.js";
+// import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/user.js";
-import chatRoute from "./routes/chat.js";
+// import chatRoute from "./routes/chat.js";
 
-dotenv.config({
-  path: "./.env",
-});
-const mongoURI = process.env.MONGO_URI;
+// dotenv.config({
+//   path: "./.env",
+// });
+// const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 
-connectDB(mongoURI);
+// connectDB(mongoURI);
 
 const app = express();
 
 // Using Middlewares Here
-app.use(express.json());
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(cookieParser());
 
 app.use("/user", userRoute);
-app.use("/chat", chatRoute);
+// app.use("/chat", chatRoute);
 
-app.use(errorMiddlewares);
+// app.use(errorMiddlewares);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
