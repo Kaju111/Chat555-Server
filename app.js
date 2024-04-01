631da324bd3819c4054ddc3ca8361f3f296c8a1f
kaju111 <saikiakaju275@gmail.com>
2024-04-01 20:27:46 +0530

    Done

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import express from "express";
-// import { connectDB } from "./utils/features.js";
-// import dotenv from "dotenv";
+import { connectDB } from "./utils/features.js";
+import dotenv from "dotenv";
 // import { errorMiddlewares } from "./middlewares/error.js";
 // import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/user.js";
 // import chatRoute from "./routes/chat.js";
 
-// dotenv.config({
-//   path: "./.env",
-// });
-// const mongoURI = process.env.MONGO_URI;
+dotenv.config({
+  path: "./.env",
+});
+const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 
-// connectDB(mongoURI);
+connectDB(mongoURI);
 
 const app = express();
 
